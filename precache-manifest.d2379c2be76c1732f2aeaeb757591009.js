aa716d19ba4466e6a6dd71d08e8187f31414aca6
Your Name <jamesmeier94@gmail.com>
2019-11-09 11:00:28 -0500

    Updates

---
+++ precache-manifest.d2379c2be76c1732f2aeaeb757591009.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d50442442d2d0a3f0720",
+    "url": "/react/static/css/main.7bce7fee.chunk.css"
+  },
+  {
+    "revision": "d50442442d2d0a3f0720",
+    "url": "/react/static/js/main.d5044244.chunk.js"
+  },
+  {
+    "revision": "a4e76a5c2ae32b239f8d",
+    "url": "/react/static/js/1.a4e76a5c.chunk.js"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "711948d2cefab8553a163241b3ab1711",
+    "url": "/react/index.html"
+  }
+];
