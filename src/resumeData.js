a07805cb0a6d1380b351c4d7225e25a49538a276
Your Name <jamesmeier94@gmail.com>
2019-11-09 10:59:03 -0500

    L

---
+++ src/resumeData.js
@@ -0,0 +1,119 @@
+let resumeData = {
+	imagebaseurl: 'https://jamesmeier.github.io/',
+	name: 'James Meier',
+	role: 'Fullstack Developer',
+	email: ' jamesmeier94@gmail.com',
+	phone: ' 404 922 1596',
+	// skypeid: 'Your skypeid',
+	roleDescription:
+		' Forward-thinking with a versatile skill set specializing in web development. Actively seeking  to earn the trust of employers and colleagues. Aiming to deliver results with the right quality and in a timely fashion. Endeavor to learn more and enhance my technological abilities in a team or sole worker.',
+	socialLinks: [
+		{
+			name: 'linkedin',
+			url: 'https://www.linkedin.com/in/james-meier-252961b7/',
+			className: 'fa fa-linkedin'
+		},
+		{
+			name: 'github',
+			url: 'https://github.com/jamesMeier',
+			className: 'fa fa-github'
+		}
+		// {
+		// 	name: 'skype',
+		// 	url: 'http://twitter.com/rbhatia46',
+		// 	className: 'fa fa-twitter'
+		// }
+	],
+	aboutme:
+		'I am a teaching assistant at The Georgia Institute of Technology in a full stack web development Bootcamp. I am a Full Stack Web Developer, currently diving deeper into Data Science via my own resources. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.',
+	address: 'jamesmeier94@gmail.com',
+	website: '4049221596',
+	education: [
+		{
+			UniversityName: 'The Georgia Insitute of Technology',
+			specialization: 'Full Stack Flex Web Bootcamp Certification',
+			MonthOfPassing: 'November',
+			YearOfPassing: '2018'
+			// Achievements: 'Some Achievements'
+		}
+		// {
+		// 	UniversityName: 'Some University',
+		// 	specialization: 'Some specialization',
+		// 	MonthOfPassing: 'Jan',
+		// 	YearOfPassing: '2018',
+		// 	Achievements: 'Some Achievements'
+		// }
+	],
+	work: [
+		{
+			CompanyName: 'Trilogy Education Services',
+			specialization: 'Teaching Assistant',
+			MonthOfLeaving: 'Present',
+			YearOfLeaving: 'Present',
+			Achievements: 'Some Achievements'
+		},
+		{
+			CompanyName: 'InstaStaff',
+			specialization: 'Contract Junior Developer',
+			MonthOfLeaving: 'June',
+			YearOfLeaving: '2019',
+			Achievements: 'Some Achievements'
+		},
+		{
+			CompanyName: 'SnapMedTech, inc',
+			specialization: 'Contract Junior Developer',
+			MonthOfLeaving: 'December',
+			YearOfLeaving: '2018',
+			Achievements: 'Some Achievements'
+		}
+	],
+	skillsDescription: 'My Stack',
+	skills: [
+		{
+			skillname: 'MongoDB'
+		},
+		{
+			skillname: 'Expressjs'
+		},
+		{
+			skillname: 'Reactjs'
+		},
+		{
+			skillname: 'Nodejs'
+		}
+	],
+	portfolio: [
+		{
+			name: 'project1',
+			description: 'mobileapp',
+			imgurl: 'images/portfolio/coffee.jpg'
+		},
+		{
+			name: 'project2',
+			description: 'mobileapp',
+			imgurl: 'images/portfolio/console.jpg'
+		},
+		{
+			name: 'project3',
+			description: 'mobileapp',
+			imgurl: 'images/portfolio/into-the-light.jpg'
+		},
+		{
+			name: 'project4',
+			description: 'mobileapp',
+			imgurl: 'images/portfolio/retrocam.jpg'
+		}
+	],
+	testimonials: [
+		{
+			description: 'This is a sample testimonial',
+			name: 'Some technical guy'
+		},
+		{
+			description: 'This is a sample testimonial',
+			name: 'Some technical guy'
+		}
+	]
+};
+
+export default resumeData;
