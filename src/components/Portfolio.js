57fb07de53c4360f4c4f57f6f1dea646f5bdbb0e
Your Name <jamesmeier94@gmail.com>
2020-01-04 16:08:43 -0500

    adding projects/ fa icons

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,36 +1,86 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 export default class Porfolio extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
       <section id="portfolio">
-      <div className="row">
-        <div className="twelve columns collapsed">
-          <h1>Check Out Some of My Works.</h1>
-          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-          {
-            resumeData.portfolio && resumeData.portfolio.map((item)=>{
-              return(
-                <div className="columns portfolio-item">
-                  <div className="item-wrap">
-                    <a href="#modal-01">
-                      <img src={`${item.imgurl}`} className="item-img"/>
-                      <div className="overlay">
-                        <div className="portfolio-item-meta">
-                          <h5>{item.name}</h5>
-                          <p>{item.description}</p>
+        <div className="row">
+          <div className="twelve columns collapsed">
+            <h1>Check Out Some of My Works.</h1>
+            <div
+              id="portfolio-wrapper"
+              className="bgrid-quarters s-bgrid-thirds cf"
+            >
+              {resumeData.portfolio &&
+                resumeData.portfolio.map(item => {
+                  return (
+                    <div className="columns portfolio-item">
+                      <div className="item-wrap">
+                        <img src={`${item.imgurl}`} className="item-img" />
+                        <a href={item.site}>
+                          <div className="overlay">
+                            <div className="portfolio-item-meta">
+                              <h5>{item.name}</h5>
+                              <p>{item.description}</p>
+                            </div>
+                          </div>
+                        </a>
+                      </div>
+                      <div className="columns portfolio-item">
+                        <div className="portfolio-wrapper">
+                          {/* <a
+                            className="fa fa-github"
+                            target="_blank"
+                            href={item.github}
+                          ></a>
+
+                          <a
+                            className="fa fa-code"
+                            target="_blank"
+                            href={item.site}
+                          ></a> */}
                         </div>
                       </div>
-                    </a>
-                  </div>
-                </div>
-              )
-            })
-          }
+                      <div className="columns portfolio-item">
+                        <div className="portfolio-wrapper">
+                          <a
+                            className="fa fa-github"
+                            target="_blank"
+                            href={item.github}
+                          ></a>
+                        </div>
+                      </div>
+                      <div className="columns portfolio-item">
+                        <div className="portfolio-wrapper">
+                          <a
+                            className="fa fa-code"
+                            target="_blank"
+                            href={item.site}
+                          ></a>
+                        </div>
+                      </div>
+                      <div className="columns portfolio-item">
+                        <div className="portfolio-wrapper">
+                          {/* <a
+                            className="fa fa-github"
+                            target="_blank"
+                            href={item.github}
+                          ></a>
+
+                          <a
+                            className="fa fa-code"
+                            target="_blank"
+                            href={item.site}
+                          ></a> */}
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
           </div>
         </div>
-      </div>
-  </section>
-        );
+      </section>
+    );
   }
 }
